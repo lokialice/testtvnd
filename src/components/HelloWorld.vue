95b2841bca85445588bd93a51ee40f53cd90c7aa
Loki Alice <lokialice@Mac24hvns-MacBook-Air.local>
2019-03-07 12:25:41 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,80 +4,35 @@
     <h2>Essential Links</h2>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
       </li>
       <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
       </li>
       <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
+        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
       </li>
       <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
       </li>
       <br>
       <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
       </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
       <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
       </li>
       <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
       </li>
       <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
       </li>
     </ul>
     <h1>{{ethAddress}}</h1>
@@ -85,38 +40,36 @@
 </template>
 
 <script>
-import Web3 from 'web3';
+var Web3 = require("web3");
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      ethAddress:""
-    }
+      msg: "Welcome to Your Vue.js App",
+      ethAddress: "...."
+    };
   },
-  mounted:function(){
-    window.addEventListener('load', async () => {
+  mounted: function() {
+   window.addEventListener('load', async () => {
     // Modern dapp browsers...
     if (window.ethereum) {
-        web3 = new Web3(ethereum);
+        window.web3 = new Web3(ethereum);
         try {
             // Request account access if needed
             await ethereum.enable();
-            const accounts = web3.eth.getAccounts().then(restult => {
-              this.ethAddress = restult;
-            });
+            window.web3.eth.getAccounts().then(result =>  this.ethAddress = result);
+            // Accounts now exposed
+            // window.web3.eth.sendTransaction({/* ... */});
         } catch (error) {
             // User denied account access...
         }
     }
     // Legacy dapp browsers...
     else if (window.web3) {
-        web3 = new Web3(web3.currentProvider);
-        const accounts = web3.eth.getAccounts().then(restult => {
-              this.ethAddress = restult;
-            });
+        window.web3 = new Web3(web3.currentProvider);
         // Accounts always exposed
         // window.web3.eth.sendTransaction({/* ... */});
+        window.web3.eth.getAccounts().then(result =>  this.ethAddress = result);
     }
     // Non-dapp browsers...
     else {
@@ -124,12 +77,13 @@ export default {
     }
 });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
